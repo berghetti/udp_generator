cfe174b0d8ec447fafca80f6a9d581b20d05ad21
berghetti <40703575+berghetti@users.noreply.github.com>
2024-02-28 20:28:10 -0400

    change DPDK version

--- install_dpdk.sh
+++ install_dpdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/DPDK/dpdk.git -o dpdk
-git checkout v22.11
+git checkout v23.03
 
 pushd ./dpdk
 
@@ -9,9 +9,6 @@ meson build
 meson configure -Dprefix=$PWD/build build
 ninja -C build
 sudo ninja -C build install
-
-echo "$PWD/build/lib64/" | sudo tee /etc/ld.so.conf.d/dpdk.conf
-sudo ldconfig
 
 popd
 
