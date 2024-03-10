ceec4a3fa8761cebdc4b8833c843113aa6e28287
fabricio <mayco.s.berghetti@gmail.com>
2024-03-10 12:59:55 -0600

    add path to i40e dpdk driver

--- install_dpdk.sh
+++ install_dpdk.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 git clone https://github.com/DPDK/dpdk.git -o dpdk
-git checkout v23.03
 
 pushd ./dpdk
 
-meson build
-meson configure -Dprefix=$PWD/build build
+git checkout v23.11
+
+# See https://networkbuilders.intel.com/docs/networkbuilders/power-management-enhanced-power-management-for-low-latency-workloads-technology-guide-1617438252.pdf A.9
+git apply ../dpdk_i40e.patch
+
+disable_apps='dumpcap,pdump,proc-info,test-acl,test-bbdev,test-cmdline,test-compress-perf,test-crypto-perf,test-eventdev,test-fib,test-flow-perf,test-gpudev,test-mldev,test-pipeline,test-pmd,test-regex,test-sad,test-security-perf'
+
+meson setup \
+  -Dc_args=-DRTE_LIBRTE_I40E_16BYTE_RX_DESC \
+  -Dplatform=native \
+  -Dtests=false \
+  -Dexamples='' \
+  -Ddisable_apps=$disable_apps \
+  -Dprefix=$PWD/build build
+
 ninja -C build
 ninja -C build install
 
