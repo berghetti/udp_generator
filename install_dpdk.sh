c9c3610afe94e7d8f373bfa0c173d39c0c96a2fa
fabricio <mayco.s.berghetti@gmail.com>
2024-03-15 08:53:56 -0600

    remove unsed drivers and libs from DPDK library

--- install_dpdk.sh
+++ install_dpdk.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-git clone https://github.com/DPDK/dpdk.git -o dpdk
+#path to dpdk
+ROOT_PATH=$(dirname $0)"/../deps/dpdk"
 
-pushd ./dpdk
+if [ "$1" = "clean" ]; then
+  sudo rm -rf $ROOT_PATH/build
+  exit 0
+fi
+
+git submodule update --init $ROOT_PATH
+
+pushd $ROOT_PATH
 
 git checkout v23.11
 
 # See https://networkbuilders.intel.com/docs/networkbuilders/power-management-enhanced-power-management-for-low-latency-workloads-technology-guide-1617438252.pdf A.9
 git apply ../dpdk_i40e.patch
 
-disable_apps='dumpcap,pdump,proc-info,test-acl,test-bbdev,test-cmdline,test-compress-perf,test-crypto-perf,test-eventdev,test-fib,test-flow-perf,test-gpudev,test-mldev,test-pipeline,test-pmd,test-regex,test-sad,test-security-perf'
+disable_apps='dumpcap,pdump,proc-info,test-acl,test-bbdev,test-cmdline,test-compress-perf,test-crypto-perf,test-eventdev,test-fib,test-flow-perf,test-gpudev,test-mldev,test-pipeline,test-pmd,test-regex,test-sad,test-security-perf,graph,test-dma-perf'
+
+disable_libs='node,graph,pipeline,table,port,fib,pdcp,ipsec,vhost,stack,security,sched,reorder,rib,mldev,regexdev,rawdev,power,pcapng,member,lpm,latencystats,jobstats,dispatcher,gpudev,cryptodev,distributor'
+
+disable_drivers='crypto/*,gpu/*,baseband/*,compress/*'
 
 meson setup \
   -Dc_args=-DRTE_LIBRTE_I40E_16BYTE_RX_DESC \
@@ -17,6 +29,8 @@ meson setup \
   -Dtests=false \
   -Dexamples='' \
   -Ddisable_apps=$disable_apps \
+  -Ddisable_libs=$disable_libs \
+  -Ddisable_drivers=$disable_drivers \
   -Dprefix=$PWD/build build
 
 ninja -C build
@@ -25,10 +39,9 @@ ninja -C build install
 popd
 
 # install igb_uio driver
-git clone https://dpdk.org/git/dpdk-kmods
-make -C dpdk-kmods/linux/igb_uio/
-sudo modprobe uio
-sudo insmod dpdk-kmods/linux/igb_uio/igb_uio.ko
+ROOT_PATH=$(dirname $0)"/../deps/dpdk-kmods"
+git submodule update --init $ROOT_PATH
 
-# TODO: bind interface
-# ./dpdk/usertools/dpdk-devbind.py -b igb_uio 18:00.1
+make -C $ROOT_PATH/linux/igb_uio/
+sudo modprobe uio
+sudo insmod $ROOT_PATH/linux/igb_uio/igb_uio.ko
