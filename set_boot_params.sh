76fba1d154142b5db2b874d2bba36c09a55486cd
mayco <mayco.s.berghetti@gmail.com>
2024-03-17 19:26:50 -0400

    add helper script to set boot parameters

---
+++ set_boot_params.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define the new line
+new_line='GRUB_CMDLINE_LINUX_DEFAULT="tsc=reliable isolcpus=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28 rcu_nocbs=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28 nohz_full=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28"'
+
+# Replace the line in the file
+sudo sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/$new_line/" /etc/default/grub
+
