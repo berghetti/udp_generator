3c6130f816d5c99d175f67d70ded2977803486d9
mayco <mayco.s.berghetti@gmail.com>
2024-03-17 19:30:48 -0400

    added new cmds

---
+++ Shremote/run_psp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N_CLIENTS=6
+BASE_DIR='/proj/demeter-PG0/users/fabricio/afp_tests'
+
+#for rate in {1000..5000..1000}; do
+for rate in 1000; do
+  rate=$((rate * 1000 / N_CLIENTS))
+  ./shremote.py psp_run.yml psp -- --basedir $BASE_DIR --rate $rate --wk wk2
+done
+
