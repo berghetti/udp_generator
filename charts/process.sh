9ff9736ac86eae3a37115afe8f2840feb2591593
mayco <mayco.s.berghetti@gmail.com>
2024-03-17 18:58:23 -0400

    fix processing

--- charts/process.sh
+++ charts/process.sh
@@ -7,34 +7,36 @@ pushd $BASE_DIR
 # join individual client results
 
 CLIENT_COUNT=$(ls -l ./client[0-9.] | grep -c ^d)
+echo "Client counting: ${CLIENT_COUNT}"
 
-# for each client rate
 for pol in client0/tests/*/*/*; do
-  rate=$(ls $pol)
-  rate_total=$((rate * CLIENT_COUNT))
-
   #remove client0/ of pol
   general_folder="${pol//client0\//}"
 
-  # runs using same rate
-  runs=$(ls $pol/$rate/test[0-9.] | wc -l)
+  echo $general_folder
 
-  #echo $rate
-  #echo $general_folder
-  echo "Creating $general_folder"
+  # for each client rate
+  rates=$(ls $pol)
+  for rate in $rates; do
+    rate_total=$((rate * CLIENT_COUNT))
+    echo "Creating $general_folder/$rate_total"
 
-  mkdir -p $general_folder/$rate_total
+    mkdir -p $general_folder/$rate_total
 
-  for i in $(seq 0 $((runs-1))); do
-    # concat requests
-    cat client[0-9.]/$general_folder/$rate/test$i > $general_folder/$rate_total/test$i
+    # runs using same rate
+    runs=$(ls $pol/$rate/test[0-9.] | wc -l)
 
-    # sum reached rps
-    offered=$(cat client[0-9.]/$general_folder/$rate/test${i}_rate | awk '$2 ~ /^[0-9]+$/ {sum += $1} END {print sum}')
-    reached=$(cat client[0-9.]/$general_folder/$rate/test${i}_rate | awk '$2 ~ /^[0-9]+$/ {sum += $2} END {print sum}')
+    for i in $(seq 0 $((runs-1))); do
+      # concat requests
+      cat client[0-9.]/$general_folder/$rate/test$i > $general_folder/$rate_total/test$i
 
-    echo -e "offered\treached\n$offered\t$reached" > $general_folder/$rate_total/test${i}_rate
+      # sum reached rps
+      offered=$(cat client[0-9.]/$general_folder/$rate/test${i}_rate | awk '$2 ~ /^[0-9]+$/ {sum += $1} END {print sum}')
+      reached=$(cat client[0-9.]/$general_folder/$rate/test${i}_rate | awk '$2 ~ /^[0-9]+$/ {sum += $2} END {print sum}')
 
+      echo -e "offered\treached\n$offered\t$reached" > $general_folder/$rate_total/test${i}_rate
+
+    done
   done
 
 done
@@ -44,7 +46,7 @@ popd
 # finally compute tail latency for each policy
 for pol in $BASE_DIR/tests/*/*/*; do
 
-  #echo $pol
+  echo $pol
   ./process.py 'wk2' p999 $pol
 
 done
