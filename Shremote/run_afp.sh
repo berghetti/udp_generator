402b6aa4f0a2055a0b2f526bdd7793429be22680
mayco <mayco.s.berghetti@gmail.com>
2024-03-17 20:24:25 -0400

    .

--- Shremote/run_afp.sh
+++ Shremote/run_afp.sh
@@ -3,8 +3,8 @@
 N_CLIENTS=6
 BASE_DIR='/proj/demeter-PG0/users/fabricio/afp_tests'
 
-for rate in {1000..5000..1000}; do
-#for rate in 1000; do
+#for rate in {1000..5000..1000}; do
+for rate in 1000; do
   rate=$((rate * 1000 / N_CLIENTS))
   ./shremote.py afp_run.yml afp -- --basedir $BASE_DIR --rate $rate --wk wk2
 done
