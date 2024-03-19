c48959c44712ed964617be032b119b79bc11acd9
mayco <mayco.s.berghetti@gmail.com>
2024-03-19 10:36:45 -0400

    change wait_timeout"

--- util.c
+++ util.c
@@ -277,8 +277,9 @@ void wait_timeout() {
   }
 
   // wait for remaining
-  // t0 = rte_rdtsc_precise();
-  // while((rte_rdtsc() - t0) < (10 * 1000000 * TICKS_PER_US)) { }
+  t0 = rte_rdtsc_precise();
+  while ((rte_rdtsc() - t0) < (10 * 1000000 * TICKS_PER_US))
+    ;
 
   // set quit flag for all internal cores
   quit_rx = 1;
