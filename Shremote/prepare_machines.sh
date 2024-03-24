cd3395e8b514e2b3639e686b118fa3fde1600224
mayco <mayco.s.berghetti@gmail.com>
2024-03-24 18:05:13 -0400

    remove scripts

--- Shremote/prepare_machines.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#./shremote.py prepare_machines.yml prepare_machines -- --psp
-#./shremote.py prepare_machines.yml prepare_machines -- --afp
-./shremote.py prepare_machines.yml prepare_machines -- --clients
-#./shremote.py prepare_machines.yml prepare_machines -- --setup_clients
-
