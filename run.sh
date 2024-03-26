2b74bcc0aa9132e7420e592bad9bfc9144ac76ba
mayco <mayco.s.berghetti@gmail.com>
2024-03-26 11:47:17 -0400

    minor improvements run.sh

--- run.sh
+++ run.sh
@@ -2,67 +2,56 @@
 
 source ./run_common.sh
 
-run_w1()
-{
-  set_w1
-  policy=$1
-  rate=$2
-  $rand=$3
-  i=$4
-  load_name='0.5_500'
-  dist='exponential'
-
-  test_dir="${dist}/${load_name}/${policy}/${rate}"
-  echo "Runing ${policy} with rate ${rate}"
-  run_one $test_dir $dist $rate $rand $i
-}
-
-run_w2()
-{
-  set_w2
-  policy=$1
-  rate=$2
-  $rand=$3
-  i=$4
-  load_name='1_100'
-  dist='exponential'
-
-  test_dir="${dist}/${load_name}/${policy}/${rate}"
-  echo "Runing ${policy} with rate ${rate}"
-  run_one $test_dir $dist $rate $rand $i
-}
-
-run_shorts()
-{
-  set_only_shorts
-  policy=$1
-  rate=$2
-  $rand=$3
-  i=$4
-  load_name='shorts_1'
-  dist='exponential'
-
-  test_dir="${dist}/${load_name}/${policy}/${rate}"
-  echo "Runing ${policy} with rate ${rate}"
-  run_one $test_dir $dist $rate $rand $i
-}
-
-set_classification_time 0
-
 policy=$2
 rate=$3
 wk=$4
 rand=$5
 test_i=$6
 
+set_classification_time 0
+
+echo "Runing ${policy} with rate ${rate}"
+
+run_w1()
+{
+  set_w1
+  load_name='0.5_500'
+  dist='exponential'
+
+  test_dir="${dist}/${load_name}/${policy}/${rate}"
+  run_one $test_dir $dist $rate $rand $test_i
+}
+
+run_w2()
+{
+  set_w2
+  load_name='1_100'
+  dist='exponential'
+
+  test_dir="${dist}/${load_name}/${policy}/${rate}"
+  run_one $test_dir $dist $rate $rand $test_i
+}
+
+run_shorts()
+{
+  set_only_shorts
+  load_name='shorts_1'
+  dist='exponential'
+
+  test_dir="${dist}/${load_name}/${policy}/${rate}"
+  run_one $test_dir $dist $rate $rand $test_i
+}
+
+
+
 if [ "$wk" = "wk1" ]; then
-  run_w1 $policy $rate $rand $test_i
+  run_w1
 fi;
 
 if [ "$wk" = "wk2" ]; then
-  run_w2 $policy $rate $rand $test_i
+  run_w2
 fi;
 
 if [ "$wk" = "shorts" ]; then
-  run_shorts $policy $rate $rand $test_i
+  run_shorts
 fi;
