b0a2c9747508d4f4902a68f8c37e0881a95701c6
fabricio <fabricio@node1.fabricio2.demeter-pg0.clemson.cloudlab.us>
2024-12-07 17:13:47 -0500

    minor change

--- run/run_test.sh
+++ run/run_test.sh
@@ -5,6 +5,26 @@ set -e
 
 source $(dirname $0)/../run/common.sh
 
+SV_IP=130.127.134.16
+
+stop_server()
+{
+  if [[ $1 == *"afp"* ]]; then
+    ssh fabricio@$SV_IP 'sudo pkill -9 fake-app;'
+  fi
+
+}
+
+restart_server()
+{
+  echo Restating server
+
+  if [[ $1 == *"afp"* ]]; then
+    ssh fabricio@$SV_IP 'sudo pkill -9 fake-app; make run -C afp/apps/fake/' &
+  fi
+
+}
+
 N_CLIENTS=1
 N_TESTS=1
 BASE_DIR='/proj/demeter-PG0/users/fabricio/afp_tests'
@@ -23,9 +43,9 @@ case $WK in
 esac
 
 # create RPS[] based on TOT_WORKER and AVG_SERVICE_TIME
-create_rps_array 1 7 3
-create_rps_array 10 60 5 
-create_rps_array 65 90 5
+create_rps_array 1 6 5
+#create_rps_array 10 90 10
+#create_rps_array 65 90 5
 echo ${RPS[@]}
 
 RANDOMS=(7 365877 374979 853172 908081 227836 64991 493663 174817 73997)
@@ -35,12 +55,18 @@ for rate in ${RPS[@]}; do
   RATE=$((rate / N_CLIENTS)) # per client rate
 
   for i in $(seq 0 $((N_TESTS-1))); do
+    #restart_server $POLICY
+    #sleep 20
+
+    echo "Starting client"
     $(dirname $0)/run.sh $BASE_DIR $POLICY $RATE $WK ${RANDOMS[$i]} $i
+
     if [ $? -ne 0 ]; then
       echo "Error test"
       exit 1
     fi
-    sleep 10
   done
 done
+
+stop_server $POLICY
 
