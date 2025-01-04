35e9d51520b51a405a1addf7a3ba964310f1fc40
fabricio <fabricio@node1.fabricio.demeter-pg0.clemson.cloudlab.us>
2025-01-04 10:59:25 -0500

    added helper script to bkp results

---
+++ process/bkp_meta.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+BASE_DST=~/afp-experiments
+
+for meta in $(ls *meta.dat*); do
+  APP_NAME=$(echo $meta | cut -d '_' -f 1)
+  LOAD_NAME=$(echo $meta | cut -d '_' -f 2)
+
+  DIR=$BASE_DST/$LOAD_NAME/$APP_NAME/$DATE
+
+  mkdir -p $DIR
+  cp $meta $DIR
+done
