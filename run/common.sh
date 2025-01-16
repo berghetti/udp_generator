a1c992ab649dc5e807244aebc281db947991cad3
fabricio <fabricio@node1.fabricio.demeter-pg0.clemson.cloudlab.us>
2025-01-16 12:41:48 -0500

    using 512 flows

--- run/common.sh
+++ run/common.sh
@@ -62,7 +62,7 @@ run_one()
   -l ${CPUS} -- \
   -d ${DIST} \
   -r ${RATE} \
-  -f 256 -s 128 -t 60 \
+  -f 512 -s 128 -t 60 \
   -c ${CONF_FILE} \
   -o ${DIR}/test$TEST_N \
   -x ${RAND} > ${DIR}/stats$TEST_N
