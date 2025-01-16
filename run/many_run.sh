0c59d48f0ea2a54a8aaf6d9a888038926c86f97f
fabricio <fabricio@node1.fabricio.demeter-pg0.clemson.cloudlab.us>
2025-01-16 12:41:32 -0500

    added new policys

--- run/many_run.sh
+++ run/many_run.sh
@@ -14,11 +14,32 @@ process_test()
 #  done
 #done
 
-POLICYS=("rss-ci" "rss-ws" "rss-ws-ci" "rss-ws-ci-wq" "rss-ws-ci-wq-cp"
-"rss-ws-ci-wq-cp-feed+qa" "rss-ws-ci-wq-cp-feed+qa+tw")
+#POLICYS=(
+#  "rss"
+#  "rss-ci"
+#  "rss-ws"
+#  "rss-ws-ci"
+#  "rss-ws-ci-wq"
+#  "rss-ws-ci-wq-cp"
+#  "rss-ws-ci-wq-cp-feed-qa"
+#  "rss-ws-ci-wq-cp-feed-qa-tw"
+#)
+
+POLICYS=(
+  "rss-ws-ci-wq-q2"
+)
 
 
 for wk in high; do
+  #for pol in ${POLICYS[@]:2}; do
+  for pol in ${POLICYS[@]}; do
+    echo $wk $pol
+    ./run_test.sh $pol $wk
+    process_test $wk $pol
+  done
+done
+
+for wk in extreme; do
   for pol in ${POLICYS[@]}; do
     echo $wk $pol
     ./run_test.sh $pol $wk
